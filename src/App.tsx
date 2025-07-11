2f07020a9e865ea285d47afca46448fe2eb07e33
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-07-11 22:33:46 +0530

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black text-[#d2fc04] font-bold text-4xl">
-      Flam Calendar
+    <div className="flex items-center justify-center h-screen bg-black">
+      <Button className="bg-[#d2fc04] font-bold text-lg cursor-pointer hover:bg-[#b3d200]">
+        Flam Calendar
+      </Button>
     </div>
   );
 }
