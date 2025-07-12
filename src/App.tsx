14901f360e1bcdc6af41ebd1b27b3c48a16879b5
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-07-12 14:34:10 +0530

    initial static layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { Button } from "./components/ui/button";
+import Calender from "@/app/Calender";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <Button className="bg-[#d2fc04] font-bold text-lg cursor-pointer hover:bg-[#b3d200]">
-        Flam Calendar
-      </Button>
-    </div>
-  );
+  return <Calender />;
 }
 
 export default App;
