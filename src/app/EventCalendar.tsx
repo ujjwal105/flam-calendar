308be714d9eddc8e58d62c397f9c73ceeff173b8
ujjwal105 <ujjwaltyagi2969@gmail.com>
2025-07-12 15:34:08 +0530

    add dummy day columns

--- src/app/EventCalendar.tsx
+++ src/app/EventCalendar.tsx
@@ -4,8 +4,12 @@ import { Funnel, Menu, Plus } from "lucide-react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 function EventCalendar() {
+  
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dummyDays = Array.from({ length: 35 }, (_, i) => i + 1);
+
   return (
-    <div className="flex">
+    <div className="flex h-screen w-full">
       <div className="w-2/9 bg-white p-8 rounded shadow flex flex-col">
         <Tabs defaultValue="month" className="w-full">
           <div className="border-b border-gray-200 mb-2">
@@ -28,7 +32,7 @@ function EventCalendar() {
           </div>
         </Tabs>
       </div>
-      <div className="w-7/9 bg-white px-6 py-4 rounded shadow h-[80vh] overflow-y-auto flex flex-col">
+      <div className="w-7/9 bg-white px-6 py-4 rounded shadow overflow-y-auto flex flex-col">
         <div className="sticky top-0 bg-white z-10 flex items-center justify-between w-full py-4 border-b border-gray-200 pb-4">
           <h1 className="text-2xl font-bold text-gray-700">August 2025</h1>
           <div className="flex items-center gap-3">
@@ -46,6 +50,24 @@ function EventCalendar() {
               <Plus /> Add Schedule
             </Button>
           </div>
+        </div>
+        <div className="grid grid-cols-7 gap-px mt-4 bg-gray-200 text-gray-600 text-sm font-semibold border">
+          {daysOfWeek.map((day) => (
+            <div key={day} className="py-3 bg-slate-100 px-auto text-center">
+              {day}
+            </div>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-7 gap-px bg-gray-200 text-sm text-gray-700 border">
+          {dummyDays.map((day, idx) => (
+            <div
+              key={idx}
+              className="bg-white min-h-24 h-28 p-2 text-left hover:bg-slate-50 cursor-pointer"
+            >
+              <span className="font-semibold text-sm text-gray-500">{day}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
